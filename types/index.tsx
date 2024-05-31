62b1e4450548c9f070de4f6a922225b89c03122b
khaled9912 <khaledmourad23345@gmail.com>
2024-05-31 23:49:14 +0300

    adding navbar and footer

--- types/index.tsx
+++ types/index.tsx
@@ -1,6 +1,6 @@
 import { MouseEventHandler } from "react";
 
-export interface CustomButtonProps {
+export interface CustomButtonProps { // interface is to define how a specific structure should look like.
     title: string;
     containerStyles?: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
