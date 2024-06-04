26ad64a914653ddd60199b35d3ed6f3c513e7313
khaled9912 <khaledmourad23345@gmail.com>
2024-06-04 05:24:45 +0300

    fixing style of hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,18 +16,17 @@ const Hero = () => {
                 streamline your car rental experience with our effortless booking process.
             </p>
             <CustomButton 
-                title = "Explore Cars"
-                containerStyles="bg-primary-blue text-white rounded-full mt-10"
-                handleClick={handleScroll}
-
+                  title="Explore Cars"
+                  containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                  handleClick={handleScroll} btnType={"button"}
             />
+            </div>
             <div className="hero__image-container">
                 <div className="hero__image">
                     <Image src="/hero.png" alt="hero" fill className="object-contain"/>
+                </div>
                     <div className="hero__image-overlay" />
 
-                </div>
-            </div>
         </div>
 
 
